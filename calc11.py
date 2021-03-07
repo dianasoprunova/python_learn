8e92337da4b8fa063995fc9de91c6c8736a0643e
Диана <dianasoprunova99@gmail.com>
2021-03-07 21:32:08 +0300

    calc

---
+++ calc11.py
@@ -0,0 +1,33 @@
+#simple div
+str_input = input("a: ")
+
+a = float(str_input)
+#print(type(a))
+
+operation = input ("+ / * - ^: ")
+
+str_input2 = input("b: ")
+b = float(str_input2)
+#print(type(b))
+result = None
+
+if operation == '+':
+ result = a / b
+#print(type(result))
+elif operation == '/':
+    if b != 0: 
+        result= a / b
+    else:
+        result= 'inf'
+elif operation == '-':
+ result = a - b
+elif operation == '*':
+ result = a * b
+elif operation == '^':
+ result = a ** b
+
+else:
+ result = "unknown"
+#print(type(result))
+
+print("Result: " + str(result))
---
+++ calcc.py
@@ -0,0 +1,65 @@
+#simple div
+
+str_command = input("Plese type command a + b or a - b: ")
+
+Znachenie1 = ''
+str_A = ''
+
+Znachenie2 = ''
+str_B = ''
+
+operation = ''
+
+for letter in str_command:
+    print(letter)
+    if letter == '+' or letter == '-' or letter == '*'or letter == '^' or letter == '/':
+        if str_A == '':
+            Znachenie1 = letter
+        elif operation != '':
+            Znachenie2 = letter
+        else:
+            operation = letter
+    else:
+        if operation == '':
+            str_A = str_A + letter
+        else:
+            str_B = str_B + letter
+
+str_A = Znachenie1 + str_A.strip()
+str_B = Znachenie2 + str_B.strip()           
+        
+
+#str_input = input("A: ")
+
+delimoe = float(str_A)
+#print(type(delimoe))
+
+#operation = input ("+ / * - ^: ")
+
+#str_input2 = input("B: ")
+delitel = float(str_B)
+#print(type(delitel))
+result = None
+
+if operation == '/':
+ result = delimoe / delitel
+#print(type(result))
+elif operation == '/':
+    if b != 0: 
+        result= delimoe / delitel
+    else:
+        result= 'inf'
+elif operation == '-':
+ result = delimoe - delitel
+elif operation == '+':
+ result = delimoe + delitel
+elif operation == '*':
+ result = delimoe * delitel
+elif operation == '^':
+ result = delimoe ** delitel
+
+else:
+ result = "unknown"
+#print(type(result))
+
+print("Result: " + str(result))
