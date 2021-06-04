5a63a984eb9b6288ed9fe631ee3836364ea6cc2e
Диана <dianasoprunova99@gmail.com>
2021-06-05 00:44:01 +0300

    golos

--- project.py
+++ project.py
@@ -1,9 +1,20 @@
 import speech_recognition as sr
 
-
+c = []
+p=' ' 
 r = sr.Recognizer ()
-harvard = sr.AudioFile('my_audio.wav')
+harvard = sr.AudioFile('golos.wav')
 with harvard as source:
     audio = r.record(source)
-print(type(audio))     # <class 'speech_recognition.AudioData'>
-print(r.recognize_google(audio))
+#print(type(audio))     # <class 'speech_recognition.AudioData'>
+с=list(r.recognize_google(audio, language ='ru-RU'))
+#print(r.recognize_google(audio, language ='ru-RU'))
+
+for i, letter in enumerate((r.recognize_google(audio, language ='ru-RU'))):
+    if letter !=' ':
+        p = p + str(letter)
+    else:
+        c.append(p)
+        p = ' '
+c.append(p)        
+print(c)
